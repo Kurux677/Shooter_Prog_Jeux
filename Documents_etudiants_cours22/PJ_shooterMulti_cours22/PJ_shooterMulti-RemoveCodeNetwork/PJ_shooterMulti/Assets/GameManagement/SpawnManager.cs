c69043466f3d76ba37f6f4179181bd042ba9be61
Pier-Luc Nadeau <1732895@cegepba.qc.ca>
2019-11-11 16:57:31 -0500

    SemiFonctionnel

---
+++ Documents_etudiants_cours22/PJ_shooterMulti_cours22/PJ_shooterMulti-RemoveCodeNetwork/PJ_shooterMulti/Assets/GameManagement/SpawnManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class SpawnManager : NetworkBehaviour {
+    public GameObject scoreDisplayerPrefab;
+    public GameObject playerRespawnManagerPrefab;
+    public GameObject middleOscillatorPrefab;
+    public GameObject RedSide;
+    public GameObject BlueSide;
+    // Use this for initialization
+    void Start () {
+       
+    }
+    [Command]
+    public void CmdSpawnManager()
+    {
+        GameObject scoreDisplayer = (GameObject)Instantiate(scoreDisplayerPrefab);
+        NetworkServer.Spawn(scoreDisplayer);
+        GameObject playerRespawnManager = (GameObject)Instantiate(playerRespawnManagerPrefab);
+        NetworkServer.Spawn(playerRespawnManager);
+    }
+
+    [Command]
+    public void CmdSpawnOscillator()
+    {
+        GameObject middleOscilator = (GameObject)Instantiate(middleOscillatorPrefab,RedSide.transform);
+        middleOscilator.transform.localPosition = new Vector3(12.5f, 0, 23);
+        NetworkServer.Spawn(middleOscilator);
+        middleOscilator = (GameObject)Instantiate(middleOscillatorPrefab, BlueSide.transform);
+        middleOscilator.transform.localPosition = new Vector3(12.5f, 0, 23);
+        NetworkServer.Spawn(middleOscilator);
+    }
+	
+	
+    
+    
+}
---
+++ Documents_etudiants_cours22/PJ_shooterMulti_cours22/PJ_shooterMulti-RemoveCodeNetwork/PJ_shooterMulti/Assets/Player/Script/PlayerHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerHealth : NetworkBehaviour, Damagable {
+    private PlayerController playerController;
+    private ScoreDisplayer scoreDisplayer;
+    private TeamManager teamManager;
+    void Start() {
+        playerController = GetComponent<PlayerController>();
+        teamManager = GetComponent<TeamManager>();
+    }
+  
+    
+    public void DealDamage(GameObject from, int damage) {
+        CmdDealDamage(from, damage);
+    }
+
+    public override void OnStartServer()
+    {
+        scoreDisplayer = FindObjectOfType<ScoreDisplayer>();
+    }
+
+    [Command]
+    public void CmdDealDamage(GameObject from, int damage)
+    {
+        if(from != null)
+        {
+            TeamManager fromTeamManager = from.GetComponent<TeamManager>();
+            if(fromTeamManager != null)
+            {
+                if(scoreDisplayer!=null)
+                {
+                    if(fromTeamManager.GetTeam()!= teamManager.GetTeam())
+                    {
+                        scoreDisplayer.RpcAddKill(fromTeamManager.GetTeam());
+                    }
+                }
+            }
+        }
+        playerController.RpcDie();
+    }
+}
